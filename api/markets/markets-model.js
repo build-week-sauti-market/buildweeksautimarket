fad7780e434daca7221db004faf23fbfd0ab9470
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2021-03-31 15:22:06 -0700

    change join

--- api/markets/markets-model.js
+++ api/markets/markets-model.js
@@ -20,8 +20,9 @@ function findById(id){
 	return db("product_info as p_i,products as p, sellers as s")
 		.join("products as p", "p.id","=", "p_i.product_id")
 		.join("sellers as s", "s.id", "=", "p_i.seller_id")
-		.where({ id })
 		.select("p.product_name", "p_i.seller_price", "p_i.qty", "p_i.description", "s.seller_name", "s.location")
+		.where({ id })
+		
 		.first()
 }
 async function add(item){
